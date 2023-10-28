f53297084d701738212febbd21609fcb584b2359
designmomonga <info@design-momonga.jp>
2023-10-28 23:42:07 +0900

    2310282342

--- app/Http/Controllers/Admin/indexController.php
+++ app/Http/Controllers/Admin/indexController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth; // Authファサードを読み込む
+use Illuminate\Support\Facades\Auth;// Authファサードを読み込む
 
 class IndexController extends Controller
 {
